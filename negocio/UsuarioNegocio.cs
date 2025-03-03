896b202e99e49dd40738250ef568288f1895a6ef
Julian <juli_j@hotmail.com.ar>
2025-03-03 13:01:50 -0300

    ultimaActualizacios

--- negocio/UsuarioNegocio.cs
+++ negocio/UsuarioNegocio.cs
@@ -9,6 +9,31 @@ namespace negocio
 {
     public class UsuarioNegocio
     {
+        public void actualizar(Usuario user)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("Update USUARIOS set imagenPerfil = @imagen, Nombre = @nombre, Apellido = @apellido, fechaNacimiento = @fecha Where Id = @id");
+                //datos.setearParametro("@imagen", user.ImagenPerfil != null ? user.ImagenPerfil : (object)DBNull.Value);
+                datos.setearParametro("@imagen", (object)user.ImagenPerfil ?? DBNull.Value);
+                datos.setearParametro("@nombre", user.Nombre);
+                datos.setearParametro("@apellido", user.Apellido);
+                datos.setearParametro("@fecha", user.FechaNacimiento);
+                datos.setearParametro("@id", user.Id);
+                datos.ejecutarAccion();
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion(); 
+            }
+        }
+
         public int insertarNuevo(Usuario nuevo)
         {
             AccesoDatos datos = new AccesoDatos();
@@ -35,7 +60,7 @@ namespace negocio
             AccesoDatos datos = new AccesoDatos();
             try
             {
-                datos.setearConsulta("Select id, usuario, pass, adminn from USUARIOS Where usuario = @usuario And pass = @pass");
+                datos.setearConsulta("Select id, usuario, pass, adminn, imagenPerfil, nombre, apellido, fechaNacimiento from USUARIOS Where usuario = @usuario And pass = @pass");
                 datos.setearParametro("@usuario", usuario.User);
                 datos.setearParametro("@pass", usuario.Pass);
                 datos.ejecutarLectura();
@@ -43,6 +68,17 @@ namespace negocio
                 {
                     usuario.Id = (int)datos.Lector["id"];
                     usuario.Adminn = (bool)datos.Lector["adminn"];
+
+                    if (!(datos.Lector["imagenPerfil"] is DBNull))
+                        usuario.ImagenPerfil = (string)datos.Lector["imagenPerfil"];
+                    if (!(datos.Lector["nombre"] is DBNull))
+                        usuario.Nombre = (string)datos.Lector["nombre"];
+                    if (!(datos.Lector["apellido"] is DBNull))
+                        usuario.Apellido = (string)datos.Lector["apellido"];
+                    if (!(datos.Lector["fechaNacimiento"] is DBNull))
+                        usuario.FechaNacimiento = DateTime.Parse(datos.Lector["fechaNacimiento"].ToString());
+
+
 
                     return true;
                 }
